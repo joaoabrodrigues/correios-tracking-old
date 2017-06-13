b33b1b3f735f0d048844a936c3aeaa50ceaf7bc6
joaoabrodrigues <joaoabrodrigues@outlook.com>
2017-06-12 21:11:01 -0300

    Converting xml to json

--- src/main/java/com/joaoabrodrigues/rest/CorreiosTrackingService.java
+++ src/main/java/com/joaoabrodrigues/rest/CorreiosTrackingService.java
@@ -3,23 +3,31 @@ package com.joaoabrodrigues.rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+
+import org.json.JSONObject;
+import org.json.XML;
 
 import com.joaoabrodrigues.integration.CorreiosIntegration;
 
 @Path("/track")
 public class CorreiosTrackingService {
 
-    @GET
+    private static final int PRETTY_PRINT_INDENT_FACTOR = 4;
+
+	@GET
     @Path("/{object}")
+    @Produces("application/json")
     public Response trackingObject(@PathParam("object") String object) {
         try {
-            String correiosResponse = CorreiosIntegration.trackObject(object);
-
-            return Response.status(200).entity("It works! -> " + correiosResponse).build();
+            JSONObject json = XML.toJSONObject(CorreiosIntegration.trackObject(object));
+            String prettyJson = json.toString(PRETTY_PRINT_INDENT_FACTOR);
+            
+            return Response.status(200).entity(prettyJson).build();
         } catch (Exception e) {
             e.printStackTrace();
-            return Response.status(400).entity("Doesn't work :(").build();
+            return Response.status(500).entity(String.format("Didn't work :( \n Error: %s ", e.getMessage())).build();
         }
     }
 
